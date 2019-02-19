b3a29e018b157700579f232c4be48ece5f0c70c6
jaysilent <korostelev.maksim@gmail.com>
2019-02-19 23:41:50 +0300

    решение

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,21 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+function getMinMax(string) {
+    let findNumber,
+        max,
+        min;
 
+    while( string ){
+        if (findNumber = parseFloat( string )){
+            max = (!max || findNumber > max) ? findNumber : max ;
+            min = (!min || findNumber < min) ? findNumber : min ;
+            string = string.substring( findNumber.toString().length + 1 ).trim();
+        } else {
+            string = string.substring(1).trim();
+        }
+    }
+
+    return {min: min, max: max};
 }
 
