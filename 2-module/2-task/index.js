5e6a637c6f8bfd13f56929356b1bb8e63fb691bc
jaysilent <korostelev.maksim@gmail.com>
2019-02-24 19:10:46 +0300

    решение

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,38 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+function find(obj, value){
+
+    let arrValue = [],
+        foundValue;
+
+    for ( let key in obj ) {
+
+        if( typeof obj[key] === 'object' ) {
+
+            let _find;
+
+            if( _find = find( obj[key], value ) )
+                arrValue.push( key + '.' + _find);
+
+            //console.log(_find, arrValue);
+
+        } else {
+
+            if( obj[key] === value ) {
+                foundValue = key;
+            }
+
+        }
+
+    }
+
+    if( arrValue.length > 0 ){
+        return ( arrValue.length === 1 ) ? arrValue.join() : arrValue;
+    } else if(foundValue !== undefined) {
+        return foundValue;
+    } else {
+        return null
+    }
+
 }
