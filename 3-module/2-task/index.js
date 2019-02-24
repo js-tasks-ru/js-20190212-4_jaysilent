af09f8046769b2dfb0234ab47c1cdbea1fb4839c
jaysilent <korostelev.maksim@gmail.com>
2019-02-25 00:09:47 +0300

    Решение

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,29 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+
+    let current = this.from;
+    let end = this.to;
+
+    return {
+        next() {
+
+            if( current < end ) {
+
+                current.setDate( current.getDate() + 1 );
+
+                let result = ( ~[0,6].indexOf(current.getDay()) ) ? `[${current.getDate()}]` : `${current.getDate()}` ;
+
+                return { done: false,
+                    value: result };
+
+            } else {
+                return { done: true };
+            }
+
+        }
+    }
+
 };
 
 
